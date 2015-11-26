244a8b40ac21a5466a861a2e0841f1288d0ec42a
orchumandz <orchumand@gmail.com>
2015-11-26 00:21:45 -0500

    upload & download data test

--- Scavenger/app/src/main/java/com/android9033/scavenger/scavenger/Control/ParseApplication.java
+++ Scavenger/app/src/main/java/com/android9033/scavenger/scavenger/Control/ParseApplication.java
@@ -3,6 +3,9 @@ package com.android9033.scavenger.scavenger.Control;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+
+import object.Quest;
 
 /**
  * Created by yirongshao on 11/2/15.
@@ -10,6 +13,7 @@ import com.parse.Parse;
 public class ParseApplication extends Application {
     public void onCreate(){
         super.onCreate();
+        ParseObject.registerSubclass(Quest.class);
         // Initial the Parse
         Parse.initialize(this, "G5Wr8k19kZZBUxM2LzN68Y7eR9uV6z4TfdpGjZCm",
                 "DUKYRPAh6C0nhRAO0Esp4hj33qI3tWjGWWA9nAGL");
