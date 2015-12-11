0457cf2b0ce09d2e7d289d299b64df29e64b503c
sergeydgr8 <smirnov@nyu.edu>
2015-12-11 01:19:29 -0500

    continued implementation of quest

--- Scavenger/app/src/main/java/com/android9033/scavenger/scavenger/Models/Quest.java
+++ Scavenger/app/src/main/java/com/android9033/scavenger/scavenger/Models/Quest.java
@@ -2,6 +2,7 @@ package com.android9033.scavenger.scavenger.Models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import org.json.JSONArray;
 
 /**
  * Created by sergey on 12/11/15.
@@ -14,5 +15,8 @@ public class Quest extends ParseObject
 {
     public String get_id() { return getString("objectId"); }
     public String get_name() { return getString("name"); }
-
+    public String get_city() { return getString("city"); }
+    public JSONArray get_landmark_ids() { return getJSONArray("landmarks"); }
+    public boolean is_public() { return getBoolean("public"); }
+    public int get_point_bounty() { return getInt("pointBounty"); }
 }
