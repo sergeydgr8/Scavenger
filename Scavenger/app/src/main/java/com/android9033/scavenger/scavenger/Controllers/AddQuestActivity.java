e4f644b3b73814eec14441e70f1e74768ad41d65
sergeydgr8 <smirnov@nyu.edu>
2015-12-14 08:52:18 -0500

    Started implementing "Add quest" feature

--- Scavenger/app/src/main/java/com/android9033/scavenger/scavenger/Controllers/AddQuestActivity.java
+++ Scavenger/app/src/main/java/com/android9033/scavenger/scavenger/Controllers/AddQuestActivity.java
@@ -1,11 +1,18 @@
 package com.android9033.scavenger.scavenger.Controllers;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.android9033.scavenger.scavenger.R;
 
 /**
  * Created by sergey on 12/14/15.
  */
 public class AddQuestActivity extends AppCompatActivity
 {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addquest);
+    }
 }
